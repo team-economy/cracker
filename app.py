1980bf172c047af82062840af3cdf90fd8003b28
Yuni Cho <jjoyuni@gmail.com>
2022-05-11 13:17:00 +0900

    Create a branch for test  - blog crawling added

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 
-from modules import auth, place, map
+from modules import auth, place, map, blog
 
 app = Flask(__name__)
 
 app.register_blueprint(auth.bp)
 app.register_blueprint(place.bp)
 app.register_blueprint(map.bp)
+app.register_blueprint(blog.bp)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
