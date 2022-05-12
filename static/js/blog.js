72f399c362e51612f21399f3280f087e471b4897
Yuni Cho <jjoyuni@gmail.com>
2022-05-12 17:32:48 +0900

    Update nextbutton function, incomplete  - using addEventListener

--- static/js/blog.js
+++ static/js/blog.js
@@ -1,10 +1,11 @@
+let i = 0;
+
 $(document).ready(function () {
     get_blog()
     get_blog_middle()
 })
 
 function get_blog() {
-    $('#blog-box').empty();
     $.ajax({
         type: "GET",
         url: '/blog',
@@ -12,18 +13,25 @@ function get_blog() {
         success: function (response) {
             let blogs = response["blog_list"]
             console.log(blogs)
-            for (let i = 0; i < blogs.length; i++) {
-                let blog = blogs[i]
-                let html_temp = `<div class="card" style="width: 18rem;">
-                                  <img src="${blog['img']}" class="card-img-top" alt="...">
-                                  <div class="card-body">
-                                    <a href="${blog['link']}" target="_blank" id="place_name">&nbsp&nbsp&nbsp&nbsp<b>${blog['title']}                                  </div>
-                                </div>`
-                $("#blog-box").append(html_temp);
+            const next = document.querySelector(".next")
+            next.addEventListener("click", temp_html (blogs)); {
+                $('#blog-box').empty();
+                // temp_html(blog)
+                console.log("clicked", i)
             }
-        }
+            }
     })
+
+function temp_html(blog) {
+    i++;
+    let html_temp = `<div class="card" style="width: 18rem;">
+                          <img src="${blog['img']}" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <a href="${blog['link']}" target="_blank" id="place_name">&nbsp&nbsp&nbsp&nbsp<b>${blog['title']}                                  </div>
+                        </div>`
+    $('#blog-box').append(html_temp)
 }
+
 
 function get_blog_middle() {
     $('#blog-box-middle').empty();
@@ -37,12 +45,13 @@ function get_blog_middle() {
             for (let i = 0; i < blogsmiddle.length; i++) {
                 let blogmiddle = blogsmiddle[i]
                 let temp_html = `<div>
-                                    <div><img src="static/cookie.png" width="50" height="50">
-                                    <a href="${blogmiddle['link']}" target="_blank" id="place_name">&nbsp&nbsp&nbsp&nbsp<b>${blogmiddle['title']}</b></a>
-                                       </label>
-                                </div>`
+                                <div><img src="static/cookie.png" width="50" height="50">
+                                <a href="${blogmiddle['link']}" target="_blank" id="place_name">&nbsp&nbsp&nbsp&nbsp<b>${blogmiddle['title']}</b></a>
+                                   </label>
+                            </div>`
                 $("#blog-box-middle").append(temp_html);
             }
         }
     })
+}
 }
